a838e0a2e1d06482920b9d41c80375549e18aa0c
hafssahajou <hafssahajou200@gmail.com>
2024-05-04 14:12:11 +0100

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AnnonceController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ReservationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,24 +21,67 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home1');
 });
 
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+
+Route::get('/register', function () {
+    return view('register');
+});
+Route::post('/register', [RegisterController::class, 'store']);
+// Route::post('/register', function(){
+//     return "test";
+// });
+Route::get('/login', [LoginController::class, 'show'])->name('logins');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Route::resource('categories', CategoryController::class);
+
+
+// Route::get('/Categories', [CategoryController::class, 'view'])->name('categories');
+// Route::post('/Categories', [CategoryController::class, 'create'])->name('addCategorie');
+// Route::delete('/Categories/{category}', [CategoryController::class, 'delete'])->name('deleteCategorie');
+// //still not working
+// Route::put('/Categorie', [CategoryController::class, 'update'])->name('updateCategorie');
+
+// // Route::prefix('admin')->group(function () {
+//     Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+//     Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+//     Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+//     Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+//     Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+//     Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+// });
+
+
+// Route::get('/categories', [CarCategoryController::class, 'index'])->name('categories.index');
+// Route::get('/categories/create', [CarCategoryController::class, 'create'])->name('categories.create');
+// Route::post('/categories', [CarCategoryController::class, 'store'])->name('categories.store');
+// Route::get('/categories/{category}/edit', [CarCategoryController::class, 'edit'])->name('categories.edit');
+// Route::put('/categories/{category}', [CarCategoryController::class, 'update'])->name('categories.update');
+// Route::delete('/categories/{category}', [CarCategoryController::class, 'destroy'])->name('categories.destroy');
 
 Route::get('/admin/statistiques' , function()
 {
     return view('admin.statistiques');
 });
 
-Route::get('/admin/category' , function()
-{
-    return view('admin.categories');
-});
+// Route::get('/admin/category' , function()
+// {
+//     return view('admin/categories');
+// });
 
-
-Route::get('/admin/immobilier' , function()
+Route::get('/admin/cars' , function()
 {
-    return view('admin.immobilier');
+    return view('admin.cars');
 });
 
 Route::get('/admin/users' , function()
@@ -44,14 +94,80 @@ Route::get('/admin/test' , function()
     return view('admin.test');
 });
 
-Route::get('/Auth/login' , function()
-{
-    return view('Auth.login');
-});
-
-Route::get('/client_Portfolio' , function()
-{
-    return view('client.Portfolio');
-});
 
 
+// Route::get('/annonces', [AnnonceController::class, 'viewAll'])->name('admin.annonces');
+// Route::get('/statistique', [UserController::class, 'statistics'])->name('statistique');
+// // Route::get('/Allannonces', [AnnonceController::class, 'adStats'])->name('adstats');
+// Route::get('/Allannonces', [AnnonceController::class, 'viewAll'])->name('viewAll');
+// Route::delete('/Allannonces/{annonce}', [AnnonceController::class, 'delete'])->name('deleteAd');
+// // });
+//     Route::get('/Users', [UserController::class, 'viewUsers'])->name('users');
+//     Route::get('/users/search', [UserController::class, 'searchUsers'])->name('users.search');
+//     Route::put('/ban/user/{userId}',  [UserController::class, 'banUser'])->name('ban.user');
+//     Route::put('/Unban/user/{userId}',  [UserController::class, 'unbanUser'])->name('unban.user');
+// // Route::middleware('auth', 'advertiser')->group(function () {
+// Route::get('/dashboard', [AnnonceController::class, 'viewlandlord'])->name('landlord.dashboard');
+// Route::post('/create', [AnnonceController::class, 'create'])->name('addAnnonce');
+// Route::put('/annonce/{id}', [AnnonceController::class, 'create'])->name('annonces.update');
+
+// // });
+
+
+
+// Route::post('/dashboard/search', [AnnonceController::class, 'search'])->name('search');
+// Route::get('/annonces', [AnnonceController::class, 'show'])->name('show.annonce');
+
+
+
+// Route::middleware('auth', 'banned')->group(function () {
+
+    Route::get('/home', [AnnonceController::class, 'viewClient'])->name('home');
+    // Route::get('/', [AnnonceController::class, 'viewvisiteur'])->name('home1');
+
+    Route::post('/annonce/search', [AnnonceController::class, 'viewClient'])->name('events.search');
+    Route::get('/details/{id}', [AnnonceController::class, 'showDetails'])->name('details');
+
+
+    // Route::middleware('client')->group(function () {
+        Route::get('/dashboard', [AnnonceController::class, 'viewlandlord'])->name('landlord.dashboard');
+        Route::get('/create', [AnnonceController::class, 'createAnnonce'])->name('addAnnonce');
+        Route::post('/create', [AnnonceController::class, 'create'])->name('addAnnonce');
+        Route::get('/annonce/{id}/edit', [AnnonceController::class, 'EditAnnoce'])->name('annonces.edit');
+        Route::put('/annonce/{id}/update', [AnnonceController::class, 'update'])->name('annonces.update');
+
+
+    // Route::middleware('admin')->group(function () {
+        Route::get('/Categories', [CategoryController::class, 'view'])->name('categories');
+        Route::post('/Categories', [CategoryController::class, 'create'])->name('addCategorie');
+        Route::delete('/Categories/{category}', [CategoryController::class, 'delete'])->name('deleteCategorie');
+        //still not working
+        Route::put('/Categorie', [CategoryController::class, 'update'])->name('updateCategorie');
+        Route::get('/annonces', [AnnonceController::class, 'viewAll'])->name('admin.annonces');
+        Route::get('/Users', [UserController::class, 'viewUsers'])->name('users');
+        Route::get('/users/search', [UserController::class, 'searchUsers'])->name('users.search');
+        Route::put('/ban/user/{userId}',  [UserController::class, 'banUser'])->name('ban.user');
+        Route::put('/Unban/user/{userId}',  [UserController::class, 'unbanUser'])->name('unban.user');
+        Route::get('/statistique', [UserController::class, 'statistics'])->name('statistique');
+        // Route::get('/Allannonces', [AnnonceController::class, 'adStats'])->name('adstats');
+        Route::get('/Allannonces', [AnnonceController::class, 'viewAll'])->name('viewAll');
+        Route::delete('/Allannonces/{annonce}', [AnnonceController::class, 'delete'])->name('deleteAd');
+        // Route::get('/annonces', [UserController::class, 'stats'])->name('stats');
+        Route::get('/annonces/{id}/reserve', [AnnonceController::class, 'showReserveForm'])->name('annonces.reserveForm');
+        Route::post('/annonces/{id}/reserve', [ReservationController::class, 'reserve'])->name('annonces.reserve');
+        
+            // Route::post('/annonces/reserve/{annonce}', [ReservationController::class, 'reserve'])->name('annonces.reserve');
+            // Route::post('/reservations/ticket/{id}', [ReservationController::class, 'generateTicket'])->name('annonces.ticket');
+            // Route::post('/showticket/{event}', [ReservationController::class, 'showTicket'])->name('showticket');
+            
+            
+        Route::get('/admin_annonce_ref/{id}', [AdminController::class, 'ArchiverAnnonce'])->name('admin.annonceref');
+        Route::get('/admin_annonce_acc/{id}', [AdminController::class, 'dearchiverAnnonce'])->name('admin.annonceacc');
+
+        Route::get('/admin_users_desactivate/{id}', [AdminController::class, 'desactivateUser'])->name('admin.users.desa');
+        Route::get('/admin_users_activate/{id}', [AdminController::class, 'activateUser'])->name('admin.users.acti');
+
+// Route::get('/utilisateur.index' , function()
+// {
+//     return view('utilisateur.index');
+// });
